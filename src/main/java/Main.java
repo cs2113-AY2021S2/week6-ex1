ef4b2c1f9b5aee7da118f0ffed6c8c7c5eeaa8a4
Akshay Narayan <anarayan.soc@gmail.com>
2021-02-13 18:06:37 +0800

    Create Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    //TODO: add your methods here
+
+    public static void main(String[] args) {
+        System.out.println("Adding numbers to the list");
+        addNumber(3);
+        addNumber(8);
+        addNumber(24);
+        System.out.println("The total is: " + getTotal());
+        System.out.println("8 in the list : " + isFound(8) );
+        System.out.println("5 in the list : " + isFound(5) );
+        removeNumber(8);
+        System.out.println("The total is: " + getTotal());
+    }
+
+}
